730ade425137854c0cbf497c22adb5d3c4e9e617
cXiaof <310257509@qq.com>
2019-09-11 23:19:08 +0800

    base function

--- docs/DefaultExample.js
+++ docs/DefaultExample.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import ColorPicker from '../src'
 
-const DefaultExample = () => (
-    <div className='example'>
-        <h2>Default</h2>
-        <ColorPicker />
-        <hr />
-    </div>
-)
+const DefaultExample = () => {
+    return (
+        <div className='example'>
+            <h2>Default</h2>
+            <ColorPicker onChange={handleChange} />
+            <hr />
+            <h2>Disabled</h2>
+            <ColorPicker disabled />
+        </div>
+    )
+}
+
+const handleChange = (color, event) => {
+    console.log(color, event)
+}
 
 export default DefaultExample
--- src/ColorPicker.js
+++ src/ColorPicker.js
@@ -1,13 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+import { ChromePicker } from 'react-color'
 
-const ColorPicker = (props) => {
-    const { disabled, title } = props
+const baseClass = 'rsuite-color-picker'
+
+const ColorPicker = React.memo((props) => {
+    const { disabled, title, value, defaultValue, onChange } = props
+    const [color, updateColor] = useState(defaultValue || '#00BCD4')
+    const [visible, setVisible] = useState(false)
+
+    const listener = ({ target }) => {
+        if (isOut(target)) setVisible(false)
+    }
+
+    useEffect(() => {
+        document.body.addEventListener('click', listener)
+        return () => {
+            document.body.removeEventListener('click', listener)
+        }
+    }, [])
+
+    const currentValue = value || color
     return (
-        <div className={`color-picker${disabled ? ' disabled' : ''}`}>
+        <div className={`${baseClass}${disabled ? ' disabled' : ''}`}>
             {title && <div className='label-text'>{title}</div>}
-            ColorPicker
+            <div
+                className={`${baseClass}-review`}
+                style={{ backgroundColor: currentValue }}
+                onClick={() => {
+                    if (!disabled) setVisible((show) => !show)
+                }}
+            />
+            {visible && (
+                <div className={`${baseClass}-overlay`}>
+                    <ChromePicker
+                        color={currentValue}
+                        onChange={(current) => {
+                            updateColor(current.hex)
+                        }}
+                        onChangeComplete={(current, e) => {
+                            onChange && onChange(current, e)
+                            updateColor(current.hex)
+                        }}
+                    />
+                </div>
+            )}
         </div>
     )
+})
+
+const isOut = (dom) => {
+    while (dom && dom.tagName.toUpperCase() !== 'BODY') {
+        if (dom.className.includes(baseClass)) return false
+        dom = dom.parentNode
+    }
+    return true
 }
 
 export default ColorPicker
